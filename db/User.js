58ec97d662ad95ba0d621ea7effeb8a1f413b2b6
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-03-17 10:39:06 -0700

    database table names should be plural

--- db/User.js
+++ db/User.js
@@ -1,6 +1,6 @@
 const {Sequelize, sequelize} = require('./db');
 
-const User = sequelize.define('user', {
+const User = sequelize.define('users', {
   username: Sequelize.STRING,
   password: Sequelize.STRING
 });
